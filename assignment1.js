23f1f6a350a497560d63acff6c24c9b02ddad83a
shehzi2000 <160365856+shehzi2000@users.noreply.github.com>
2024-03-04 05:58:22 +0500

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,6 @@
+"use strict";
+// question :1
+console.log("Hellow,World");
+//Question :2
+let friend = ("Hellow Shahid,would you like to learn some python today?");
+console.log(friend);
---
+++ assignment1.ts
@@ -0,0 +1,11 @@
+// question :1
+console.log("Hellow,World");
+
+
+
+
+
+//Question :2
+
+let friend =("Hellow Shahid,would you like to learn some python today?");
+console.log(friend)
