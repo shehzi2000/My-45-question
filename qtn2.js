ba019222122b802a4d50aa3e4fef82c57a2820ac
shehzi2000 <160365856+shehzi2000@users.noreply.github.com>
2024-03-06 04:21:15 +0500

    Add files via upload

---
+++ qtn2.js
@@ -0,0 +1,9 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "Shahid";
+// in lowercase
+console.log("lowercase:,", personName.toLowerCase());
+// in uppercase
+console.log("uppercase:,", personName.toUpperCase());
+// titlecas
+console.log("titlecase:,", personName.replace(/\b\w/g, c => c.toUpperCase()));
