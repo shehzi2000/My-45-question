4a3d553b4343939ad1ba5a8113da76ed19ae0608
shehzi2000 <160365856+shehzi2000@users.noreply.github.com>
2024-03-09 05:18:43 +0500

    Add files via upload

---
+++ qtn4.js
@@ -0,0 +1,4 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var person = ("Hello Shahid");
+var a = ",would you like to learn some Python today?";
+console.log(person + a);
---
+++ qtn4.ts
@@ -0,0 +1,6 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// print a string with hello to a person
+
+let person: string = ("Hello Shahid");
+let a:string =",would you like to learn some Python today?" 
+console.log(person+a)
