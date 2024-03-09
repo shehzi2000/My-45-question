12f26b612a919b4824806d17f64f35a8915ea9f4
shehzi2000 <160365856+shehzi2000@users.noreply.github.com>
2024-03-09 05:19:24 +0500

    Add files via upload

---
+++ qtn5.js
@@ -0,0 +1,5 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// print the quote of Shakespeare
+var autName = ("Shakepeare said,");
+var Quote = ' "The world is a theatre,and everyone is an actor"';
+console.log(autName + Quote);
---
+++ qtn5.ts
@@ -0,0 +1,7 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// print the quote of Shakespeare
+
+let autName:string=("Shakepeare said,")
+let Quote:string=' "The world is a theatre,and everyone is an actor"'
+console.log(autName+Quote)
