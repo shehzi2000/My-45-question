2417300c55d268002ba3844548838c9d7a42a36f
shehzi2000 <160365856+shehzi2000@users.noreply.github.com>
2024-03-11 05:19:47 +0500

    Add files via upload

---
+++ qtn6.js
@@ -0,0 +1,5 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// print the Quote by taking author name in variable
+var famous_person = ("Shakepeare said,");
+var message = ('  "The world is a theatre,and everyone is an actor"   ');
+console.log(famous_person + message);
---
+++ qtn6.ts
@@ -0,0 +1,7 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// print the Quote by taking author name in variable
+
+let famous_person:string=("Shakepeare said,")
+let message:string=('  "The world is a theatre,and everyone is an actor"   ')
+ console.log(famous_person+message);
+
