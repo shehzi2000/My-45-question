48660146b2eddfcb384c7e6340272f24abeb5934
shehzi2000 <160365856+shehzi2000@users.noreply.github.com>
2024-03-13 03:17:05 +0500

    Add files via upload

---
+++ qtn10.js
@@ -0,0 +1,8 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// console.log ('Shakepeare said,"The world is a theatre,and everyone is an actor"');
+console.log("This programme is about to print a quote of my favourite authore and his name");
+// console.log(3+5);
+// console.log(11-3);
+// console.log(2*4);
+// console.log(16/2);
+console.log("This proqramme is about the calculation where i have to '+,'-'*'/' where the answer should must be '8'");
