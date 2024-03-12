1c053673251d74a6a41c4f89c7e98d397ccbb68d
shehzi2000 <160365856+shehzi2000@users.noreply.github.com>
2024-03-13 03:16:17 +0500

    Add files via upload

---
+++ qtn9.js
@@ -0,0 +1,5 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var num1 = ("3");
+console.log(num1);
+num1 = ("This is my favourite number");
+console.log(num1);
---
+++ qtn9.ts
@@ -0,0 +1,6 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+  
+let num1=("3");
+console.log(num1)
+num1=("This is my favourite number")
+console.log(num1);
